e810dcdad78c66ff0dc3e596ede5b12a80312ab0
Vikram Cothur <vcothur7@gmail.com>
2020-06-06 12:44:59 +0530

    changes automate.py

--- automate.py
+++ automate.py
@@ -2,6 +2,7 @@ import os
 from getpass import getpass
 from sys import platform
 from subprocess import Popen, PIPE
+import pexpect
 if platform == "win32":
     commit_msg = input("COMMIT MESSAGE:")
     os.chdir(r"D:\MiscProjects\Game Projects JS\ZaPac")
@@ -12,12 +13,11 @@ else:
     password = getpass()
     os.chdir("/root/iogames/MPac")
     os.system("git stash push")
-    git = Popen('git pull origin master'.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    git.stdin.write("fire-hound\r")
-    git.stdin.write(f"{password}\r")
-    git.stdin.close()
-    for line in git.stdout:
-        print(line.strip())
+    git = pexpect.spawn("git pull origin master")
+    git.expect("Username for 'https://github.com':")
+    git.sendline("fire-hound")
+    git.expect("Password for 'https://fire-hound@github.com':")
+    git.sendline(password)
     os.system("git stash drop")
     os.system("pm2 stop index.js")
     os.system("pm2 start index.js")
